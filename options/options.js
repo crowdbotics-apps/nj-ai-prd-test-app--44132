cf8f8571f3ad68170b0301bb29a18948a523d70a
Crowdbotics <team@crowdbotics.com>
2023-10-24 17:06:49 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "nj-ai-prd-test-app--44132",
+  url: "https://nj-ai-prd-test-app--44132.botics.co",
+  api: "https://nj-ai-prd-test-app--44132.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
